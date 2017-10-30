41175a357c601ad7a5772a2a7dd90defbe1c7d24
zhao.weiwei <zhao.weiwei@jyall.com>
2017-10-30 20:11:23 +0800

    行业小程序的改造

--- src/main/java/com/jyall/feign/JerseyAdvise.java
+++ src/main/java/com/jyall/feign/JerseyAdvise.java
@@ -32,6 +32,7 @@
 */
 package com.jyall.feign;
 
+import com.jyall.annotation.EnableJersey;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,6 +41,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -54,8 +56,8 @@ import java.util.Map;
  * Email is zhao.weiwei@jyall.com
  * Copyright is 金色家园网络科技有限公司
  */
-@Component
-@ConditionalOnBean(ResourceConfig.class)
+@Configuration
+@ConditionalOnBean(annotation = EnableJersey.class)
 public class JerseyAdvise implements ApplicationContextAware {
     private Logger logger = LoggerFactory.getLogger(getClass());
     private ApplicationContext applicationContext;
