9834c571b06701688e167e0e459a3d407ab21981
zhao.weiwei <zhao.weiwei@jyall.com>
2017-10-30 21:32:17 +0800

    C层注册

--- src/main/java/com/jyall/jyctrller/JyctrllerRegister.java
+++ src/main/java/com/jyall/jyctrller/JyctrllerRegister.java
@@ -5,7 +5,9 @@ import com.netflix.appinfo.ApplicationInfoManager;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.CloudEurekaClient;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.context.ApplicationContext;
@@ -25,9 +27,10 @@ import java.util.Map;
  * zhao.weiwei@jyall.com.
  */
 @Lazy
-@Component
 @Singleton
-//@ConditionalOnProperty(name = "eureka.client.jyctrller.registered",havingValue = )
+@Component
+@AutoConfigureAfter(EmbeddedServletContainerAutoConfiguration.class)
+@ConditionalOnProperty(name = "eureka.client.jyctrller.registered", havingValue = "true")
 public class JyctrllerRegister {
 
     // 注入原有的服务注册与发现的配置
@@ -37,8 +40,8 @@ public class JyctrllerRegister {
     @Autowired
     private ApplicationInfoManager applicationInfoManager;
     // 是否注册到C层的标志，默认是不注册
-    @Value("${eureka.client.jyctrller.registered:false}")
-    private boolean shouldRegCtrller;
+//    @Value("${eureka.client.jyctrller.registered:false}")
+//    private boolean shouldRegCtrller;
     // C层注册的地址
     @Value("${eureka.client.jyctrller.registryUrls:}")
     private String ctrllerRegistryUrls;
@@ -46,21 +49,21 @@ public class JyctrllerRegister {
     @Autowired
     private ApplicationContext applicationContext;
 
-//    @PostConstruct
+    @PostConstruct
     public void register() throws Exception {
-        if (this.shouldRegCtrller) {
-            EurekaClientConfigBean bean = new EurekaClientConfigBean();
-            // 将原来的服务发现的属性copy到属性
-            BeanUtils.copyProperties(this.config, bean);
-            Map<String, String> serviceUrl = Maps.newHashMap();
-            // 将defaultZone的属性换成C层注册的URL
-            serviceUrl.put("defaultZone", this.ctrllerRegistryUrls);
-            bean.setServiceUrl(serviceUrl);
-            // 设置注册属性
-            bean.setRegisterWithEureka(true);
-            // 设置拉取服务的属性
-            bean.setFetchRegistry(false);
-            new CloudEurekaClient(this.applicationInfoManager, bean, null, applicationContext);
-        }
+//        if (this.shouldRegCtrller) {
+        EurekaClientConfigBean bean = new EurekaClientConfigBean();
+        // 将原来的服务发现的属性copy到属性
+        BeanUtils.copyProperties(this.config, bean);
+        Map<String, String> serviceUrl = Maps.newHashMap();
+        // 将defaultZone的属性换成C层注册的URL
+        serviceUrl.put("defaultZone", this.ctrllerRegistryUrls);
+        bean.setServiceUrl(serviceUrl);
+        // 设置注册属性
+        bean.setRegisterWithEureka(true);
+        // 设置拉取服务的属性
+        bean.setFetchRegistry(false);
+        new CloudEurekaClient(this.applicationInfoManager, bean, applicationContext);
+//        }
     }
 }
