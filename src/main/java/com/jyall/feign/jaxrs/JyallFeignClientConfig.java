105594a15a148ca14712c5f6029c316c045e66a1
zhao.weiwei <zhao.weiwei@jyall.com>
2017-11-02 11:40:31 +0800

    添加注解

--- src/main/java/com/jyall/feign/jaxrs/JyallFeignClientConfig.java
+++ src/main/java/com/jyall/feign/jaxrs/JyallFeignClientConfig.java
@@ -1,11 +1,8 @@
 package com.jyall.feign.jaxrs;
 
+import feign.Contract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.jyall.feign.jaxrs.JAXRSContract;
-
-import feign.Contract;
 
 /**
  * 覆盖Spring Cloud Feign Client的默认配置
