cf0b917ca6e63155542a64476be3d76d8aa46640
zhao.weiwei <zhao.weiwei@jyall.com>
2018-04-03 13:21:05 +0800

    http://www.jyall.com/favicon.ico

--- src/main/java/com/jyall/feign/jaxrs/JyallFeignClientConfig.java
+++ src/main/java/com/jyall/feign/jaxrs/JyallFeignClientConfig.java
@@ -1,8 +1,6 @@
 package com.jyall.feign.jaxrs;
 
-import com.jyall.annotation.EnableJersey;
 import feign.Contract;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
  * @author guo.guanfei
  */
 @Configuration
-@ConditionalOnBean(annotation = EnableJersey.class)
 public class JyallFeignClientConfig {
 
     @Bean
