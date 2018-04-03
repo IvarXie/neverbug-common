ad816a450068e1b9b7a5c59f3fa595b88a9588da
zhao.weiwei <zhao.weiwei@jyall.com>
2018-04-03 17:32:54 +0800

    0.0.5-SNAPSHOT

--- src/main/java/com/jyall/jersey/JerseyConfig.java
+++ src/main/java/com/jyall/jersey/JerseyConfig.java
@@ -72,8 +72,8 @@ public class JerseyConfig {
     @Bean(name = "defaultJerseyResourceConfig")
     public ResourceConfig resourceConfig() throws Exception {
         ClassPool pool = ClassPool.getDefault();
-        ClassClassPath classPath = new ClassClassPath(this.getClass());
-        pool.insertClassPath(classPath);
+        pool.insertClassPath(new ClassClassPath(ResourceConfig.class));
+        pool.insertClassPath(new ClassClassPath(ApplicationPath.class));
         CtClass cc = pool.makeClass(getClass().getPackage() + "." + UUID.randomUUID().toString().replaceAll("-", "").toUpperCase());
         CtClass superClass = pool.get(ResourceConfig.class.getName());
         cc.setSuperclass(superClass);
